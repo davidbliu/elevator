95e75d0a14beea585c978699529959db9c3cd6fe
David Liu <davidbliu@Davids-MacBook-Pro.local>
2015-03-17 01:40:37 -0700

    add direction of elevators

--- elevator_david.py
+++ elevator_david.py
@@ -10,16 +10,25 @@ with open('sample_input.txt', 'r') as ifile:
 # naive scheduling algorithm: handle requests as they come in
 time = 0
 pos = 0
-for i in inputs:
+for ind in range(0, len(inputs)):
+	i = inputs[ind]
 	floor_a = i[1]
 	floor_b = i[2]
 	t = i[3]
 	dist_a = abs(floor_a-pos)
-	print 'Move elevator 1 to floor '+str(floor_a) + ' at time '+str(time)
+	direction = 'down'
+	if floor_b>floor_a:
+		direction = 'up'
+	print 'Move elevator 1 to floor '+str(floor_a) + ' at time '+str(time) + ' going '+direction
 	time = time+dist_a
 	# if you have to wait
 	if t>time:
 		time = t
-	print 'Move elevator 1 to floor '+str(floor_b) + ' at time '+str(time)
+
+	direction = 'down'
+	if ind<len(inputs)-1 and inputs[ind+1][1] > floor_b:
+		direction = 'up'
+	# drop off at desired floor, then set the direction to go in the next floor a
+	print 'Move elevator 1 to floor '+str(floor_b) + ' at time '+str(time) + ' going '+direction
 	pos = floor_b
 	time = time+abs(floor_a-floor_b)
