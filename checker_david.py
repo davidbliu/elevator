7bdfabe76b59185c31a1c0d71fd7361ae6871b72
David Liu <davidbliu@Davids-MacBook-Pro.local>
2015-03-17 00:54:19 -0700

    sample output modified

---
+++ checker_david.py
@@ -0,0 +1,1 @@
+with open('sample_input.txt', )
--- elevator_david.py
+++ elevator_david.py
@@ -7,7 +7,7 @@ with open('sample_input.txt', 'r') as ifile:
 		inputs.append([splits[0], int(splits[3]), int(splits[9]), int(splits[-1])])
 
 
-# print out output lines
+# naive scheduling algorithm: handle requests as they come in
 time = 0
 pos = 0
 for i in inputs:
@@ -17,6 +17,9 @@ for i in inputs:
 	dist_a = abs(floor_a-pos)
 	print 'Move elevator 1 to floor '+str(floor_a) + ' at time '+str(time)
 	time = time+dist_a
+	# if you have to wait
+	if t>time:
+		time = t
 	print 'Move elevator 1 to floor '+str(floor_b) + ' at time '+str(time)
 	pos = floor_b
 	time = time+abs(floor_a-floor_b)
