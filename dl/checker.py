5bcfd978747614036985de923c943e33d336de1b
David Liu <davidbliu@Davids-MacBook-Pro.local>
2015-03-19 23:48:23 -0700

    .

--- dl/checker.py
+++ dl/checker.py
@@ -6,7 +6,11 @@ class Request:
 		self.floor1= floor1
 		self.floor2 = floor2
 		self.time = time
-
+	def dir(self):
+		if self.floor2 > self.floor1:
+			return 1
+		else:
+			return -1
 class Elevator:
 	maxfloor = 25
 	def __init__(self):
@@ -15,18 +19,29 @@ class Elevator:
 		self.requests = []
 	def move(self):
 		self.pos += self.direction
-		if pos >= maxfloor or pos < 0:
+		if self.pos >= Elevator.maxfloor or self.pos < 0:
 			raise Exception("floor outside bounds")
-	def idle(self):
+	def idle(self, time, request_pool):
+		print 'idle at '+str(self.pos)+' '+str(self.direction)
 		# check if people have to get out of elevator
-		for request in self.requests:
-			
+		fulfilled_requests = [r for r in self.requests if r.floor2 == self.pos]
+		requests = [r for r in self.requests if r.floor2 != self.pos]
+		# get new requests
+		new_requests = [r for r in request_pool if r.floor1 == self.pos and r.dir() == self.direction and r.time == time]
+		self.requests = requests+new_requests
+		new_request_pool = [r for r in request_pool if r not in self.requests]
+		
 
+		print len(fulfilled_requests)
+		print len(new_requests)
+		print len(self.requests)
+		print len(new_request_pool)
+		return {'fulfilled':fulfilled_requests, 'new_pool': new_request_pool}
+		
 ### LOAD REQUESTS INTO REQUESTS LIST
 with open('sample_input.txt', 'r') as ifile:
 	for line in ifile:
 		splits = line.rstrip().split(' ')
-		inputs.append([splits[0], int(splits[3]), int(splits[9]), int(splits[-1])])
 		req = Request(splits[0],
 				int(splits[3]),
 				int(splits[9]),
@@ -35,15 +50,27 @@ with open('sample_input.txt', 'r') as ifile:
 
 ### check inputted move sequence to see if valid
 # assume use of only one elevator
-def check_moves(moves, requests):
+def check_moves(moves, requests, elevator):
 	time = 0
 	for move in moves:
-		make_move(move, elevator, request)
-def make_move(move, elevator, requests):
+		requests = make_move(move, time, elevator, requests)
+		time += 1
+def make_move(move, time,  elevator, requests):
 	if move == 'm':
 		elevator.move()
 	if move == 'i':
 		# check if people need to get in or out of elevator
+		data = elevator.idle(time, requests)
+		requests = data['new_pool']
+		for f in data['fulfilled']:
+			print f.name
 	if move == 's':
 		elevator.direction *= -1
-	time += 1
+	return requests
+	
+elevator = Elevator()
+check_moves('mmmsimm', requests, elevator)
+
+
+
+
