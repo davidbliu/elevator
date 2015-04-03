74502a50e2fbd0834c8a819add0d029ba323edaa
David Liu <davidbliu@Davids-MacBook-Pro-3.local>
2015-04-02 21:16:37 -0700

    .

--- flask/postgres.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-
-try:
-    conn = psycopg2.connect("dbname='elevator' user='postgres' host='localhost' password='password'")
-except:
-    print "I am unable to connect to the database"
