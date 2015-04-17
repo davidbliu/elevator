3c849c67f753c945cbedd176bfd70205f1bd3365
David Liu <davidbliu@gmail.com>
2015-04-16 22:51:59 -0700

    add new solver

--- cleanup.py
+++ cleanup.py
@@ -12,13 +12,38 @@ register(application_id, client_key)
 class Score(Object):
 	pass
 
-scores = Score.Query.all()
 
-args = sys.argv[1]
+def cleanup_names(bad_names):
+	scores = Score.Query.all()
 
-badnames = set(args.split(','))
-for score in scores:
-	if score.name in badnames:
-		print 'deleting score '+str(score.name)
-		# score.delete()
-		print score.solution
+	args = sys.argv[1]
+
+	badnames = set(args.split(','))
+	for score in scores:
+		if score.name in bad_names:
+			print 'deleting score '+str(score.name)
+			score.delete()
+
+def cleanup_committees():
+	scores = Score.Query.all()
+	for score in scores:
+		name = score.name
+		bracks = name.split('[')
+		try:
+			committee = bracks[1].split(']')[0]
+			if committee != score.committee:
+				print 'setting '+score.name+"'s committee to "+committee
+				score.committee = committee
+				score.save()
+		except:
+			print name+' is uncategorized'
+
+if __name__=="__main__":
+	
+	# cleanup_committees()
+
+	bad_names = ['Ian', 'asdf', 'ok', 'K', 'incredi_tester', 'poop', 'test', 'testes', 'Marketing']
+	cleanup_names(bad_names)
+	cleanup_committees()
+	# cleanup_names(bad_names)
+	# cleanup_committees()
---
+++ flask/prune.py
@@ -0,0 +1,18 @@
+from seeds import *
+from random import shuffle
+import random
+import itertools
+from name_checker import *
+import sys
+import copy
+
+def findsubsets(S,m):
+    return set(itertools.combinations(S, m))
+
+
+
+def cost(solution, challenge):
+
+
+if __name__=="__main__":
+	print 'calculating cost of this solution'
